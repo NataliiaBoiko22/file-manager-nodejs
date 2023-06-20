320fe16e461cd88b256929344ffbcf1e3ab587ba
NataliiaBoiko22 <nata.boyko.kras@gmail.com>
2023-06-20 15:07:06 +0200

    feat: add exit function

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
+import os from "os";
+import readline from "readline";
+import { stdin, stdout, exit } from "process";
+import path from "path";
 const fileManager = async () => {
+  const homedir = os.homedir();
+  let currdir = path.join(homedir);
+
   const username = process.argv
     .slice(2)
     .filter((s) => s.includes("--username"))[0]
     .split("=")[1];
   console.log(`Welcome to the File Manager, ${username}!`);
-};
 
+  const rl = readline.createInterface({
+    input: stdin,
+    output: stdout,
+    prompt: `You are currently in ${currdir}`,
+  });
+  rl.prompt();
+  const stopFileManager = () => {
+    console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+    exit();
+  };
+
+  rl.on("SIGINT", () => {
+    stopFileManager();
+  });
+  rl.on("line", (inputLine) => {
+    if (inputLine.trim().toLowerCase().includes(".exit")) {
+      stopFileManager();
+    }
+  });
+};
 fileManager();
