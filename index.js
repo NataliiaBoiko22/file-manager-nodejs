531907a3cd32319776a2a5bb30285b52016687dd
NataliiaBoiko22 <nata.boyko.kras@gmail.com>
2023-06-20 12:45:41 +0200

    feat: add first message

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hi");
+const fileManager = async () => {
+  const username = process.argv
+    .slice(2)
+    .filter((s) => s.includes("--username"))[0]
+    .split("=")[1];
+  console.log(`Welcome to the File Manager, ${username}!`);
+};
+
+fileManager();
